4a799b6793a0e88428277ea435fb8c7e133918df
Gavrilo <Gavrilo@DESKTOP-C8F73M2>
2016-06-13 16:58:31 +0200

    Change on aktiController.js

--- src/main/webapp/module/akt/aktiController.js
+++ src/main/webapp/module/akt/aktiController.js
@@ -1,10 +1,23 @@
 app.controller('aktiController', function($scope,$state,$mdDialog,$translate, loginService, aktService){
 	$scope.init = function(){
-		alert("DADAS");
 		aktService.getAll(function(response){
 			$scope.akti = response.data;
 			console.log($scope.akti);
 		})
 	};
+	$scope.dodajNoviAkt = function(){
+		var akt = {};
+		$mdDialog.show({
+	          controller: 'aktDialogController',
+	          templateUrl: 'module/akt/aktDialog/aktDialog.html',
+	          clickOutsideToClose: true,
+	          akt: akt
+	       }).then(function(){
+	    	   ucitajAkte();
+	       });
+	}
 	
+	$scope.ucitajAkte = function(){
+		
+	}
 });
